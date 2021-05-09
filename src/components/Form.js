db89c57f56b84db7532fa7031ca5bf76b71d4dfb
sezaite <martyna.sezaite@gmail.com>
2021-05-09 12:26:20 +0300

    value error handled

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form({ Login, error }) {
+    const [details, setDetails] = useState({ email: "", password: "" });
+
+    const submitHandler = e => {
+        e.preventDefault();
+        Login(details);
+    }
     return (
-        <form>
-            <h3 className="form-header">Login to the chat</h3>
+        <form onSubmit={submitHandler}>
+            <h3 className="form-header">Иди и смотри</h3>
             <div className="form-group">
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" onChange={e => setDetails({ ...details, email: e.target.value })} value={details.email} />
             </div>
             <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
+                <input type="password" id="password" name="password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} />
             </div>
             <input type="submit" value="Enter the void" />
         </form>
