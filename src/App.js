54bb41050fe4077fb2ca9fa6d887dc5a98014e2c
sezaite <martyna.sezaite@gmail.com>
2021-05-10 22:05:23 +0300

    new code same errrors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
 import React, { useState } from 'react';
 import Form from './components/Form';
+import { validEmail, validPassword } from './components/Regex.js';
 
 function App() {
-  const [user, setUser] = useState({ name: "", email: "" });
-
-  const error = error => {
-    console.log(error);
-  }
+  const [user, setUser] = useState({ name: "", email: "", password: "" });
+  const [emailError, setEmailError] = useState(false);
+  const [passError, setPassError] = useState(false);
 
   const Login = details => {
-    console.log(details);
+    setEmailError(false);
+    setPassError(false);
+    if (!validEmail.test(details.email)) {
+      setEmailError(true);
+    }
+    if (!validPassword.test(details.password)) {
+      setPassError(true);
+    }
+    setUser({
+      name: details.name,
+      email: details.email,
+      password: details.password
+
+    });
   }
 
   const Logout = () => {
-    console.log('Lougout');
+
+    setUser({
+      name: "",
+      email: "",
+      password: ""
+    });
+    setEmailError(false);
+    setPassError(false);
+
   }
+
+  // user.name !== "" && user.email !== "" && user.password !== "" &&
 
   return (
     <div className="App">
-      {(user.email != "") ?
-        (
-          console.log('chato langas')
+      {(!setEmailError && !setPassError) ?
+        (<>
+          <h3>Logged as <span>{user.name}</span></h3>
+          <button onClick={Logout}>Exit</button>
+        </>
         )
         :
         (
-          <Form Login={Login} error={error} />
+
+          <>
+            {/* {emailError && <p>Your email is invalid</p>}
+            {passError && <p>Your password must contain 8 characters and at least one numeric value</p>} */}
+            <Form Login={Login} emailError={emailError} passError={passError} />
+          </>
         )
       }
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
-function Form({ Login, error }) {
-    const [details, setDetails] = useState({ email: "", password: "" });
+function Form({ Login, emailError, passError }) {
+    const [details, setDetails] = useState({ name: "", email: "", password: "" });
 
     const submitHandler = e => {
         e.preventDefault();
@@ -10,6 +10,12 @@ function Form({ Login, error }) {
     return (
         <form onSubmit={submitHandler}>
             <h3 className="form-header">Иди и смотри</h3>
+            {emailError && <h3 className="error">Your email is invalid</h3>}
+            {passError && <h3 className="error">Your password is invalid</h3>}
+            <div className="form-group">
+                <label htmlFor="name">Your name</label>
+                <input type="name" id="name" name="name" onChange={e => setDetails({ ...details, name: e.target.value })} value={details.name} />
+            </div>
             <div className="form-group">
                 <label htmlFor="email">Email</label>
                 <input type="email" id="email" name="email" onChange={e => setDetails({ ...details, email: e.target.value })} value={details.email} />
