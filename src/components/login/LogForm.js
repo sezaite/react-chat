f4a44b989a8e6ad29e7e0f05182fc6e3cd265e11
sezaite <martyna.sezaite@gmail.com>
2021-05-14 01:59:25 +0300

    login basic scss

--- src/components/login/LogForm.js
+++ src/components/login/LogForm.js
@@ -11,22 +11,24 @@ function LogForm({ Login, emailError, passError, emailErrorChanger, passErrorCha
     }
     return (
         <form onSubmit={submitHandler}>
-            <h3 className="form-header">Иди и смотри</h3>
+            <h3 className="form-header">Come and see</h3>
             {emailError && <h3 className="error">Your email is invalid</h3>}
             {passError && <h3 className="error">Your password must contain at least 8 characters at least 1 numeric character</h3>}
-            <div className="form-group">
-                <label htmlFor="name">Your name</label>
-                <input type="name" id="name" name="name" onChange={e => setDetails({ ...details, name: e.target.value })} value={details.name} />
+            <div className="form-body">
+                <div className="form-group">
+                    <label htmlFor="name">Your name</label>
+                    <input type="name" id="name" name="name" onChange={e => setDetails({ ...details, name: e.target.value })} value={details.name} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" onChange={e => setDetails({ ...details, email: e.target.value })} value={details.email} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} />
+                </div>
             </div>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" onChange={e => setDetails({ ...details, email: e.target.value })} value={details.email} />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} />
-            </div>
-            <input type="submit" value="Enter the void" />
+            <input type="submit" value="Enter the void" className="enter btn" />
         </form>
     )
 }
