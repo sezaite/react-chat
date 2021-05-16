041556d3f67351889a7a188ad5897d3c29b07e47
sezaite <martyna.sezaite@gmail.com>
2021-05-16 21:34:32 +0300

    clearing chat window

--- src/components/dashboard/TextForm.js
+++ src/components/dashboard/TextForm.js
@@ -5,18 +5,27 @@ function TextForm({ addMessage }) {
 
     const handleMessageSubmission = (e) => {
         e.preventDefault();
+        // e.target.value = "";
+        document.querySelector(".message-input textarea").value = "";
         addMessage(message);
+        console.log(e);
     }
 
-    function autoGrow(element) {
+    const handleKeyPress = e => {
+        if (e.key === 'Enter') {
+            handleMessageSubmission(e);
+        }
+    }
+
+    const autoGrow = element => {
         element.style.height = "5px";
         element.style.height = (element.scrollHeight) + "px";
     }
 
     return (
         <form className="message-input">
-            <textarea name="message-area" id="message-area" placeholder="Say something!" onChange={e => setMessage(e.target.value)} value={message} onInput={e => autoGrow(e.target)}></textarea>
-            <input type="submit" value="Send" onClick={handleMessageSubmission} />
+            <textarea name="message-area" id="message-area" placeholder="Say something!" onChange={e => setMessage(e.target.value)} onInput={e => autoGrow(e.target)} onKeyPress={e => handleKeyPress(e)}>{message}</textarea>
+            <input type="submit" value="Send" onClick={e => handleMessageSubmission(e)} />
 
         </form>
     )
