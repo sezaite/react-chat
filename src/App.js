619df16338b800741f1fc7bd35c3a95bc8bc38c4
sezaite <martyna.sezaite@gmail.com>
2021-07-15 08:13:11 +0300

    router base name

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="chat-app">
         <Switch>
           <Route exact path="/">
